450125f5a82c4877c06bf9cda8e12b15fb4c3637
Houston Krohman <houstonKrohman@houstons-macbook-pro.local>
2016-06-28 15:10:20 -0700

    Initial Commit

---
+++ MMH/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MMH
+//
+//  Created by Houston Krohman on 2016-06-28.
+//  Copyright © 2016 Houston Krohman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
